f6007ee149f9c9d324623ca4faeac430a1a790c7
tom sandholm <tom.sandholm@gmail.com>
2026-01-03 15:21:30 -0500

    initial import

---
+++ global-diskuse.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+#
+echo "global-diskuse" | su - ansible
---
+++ global-ping.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# vi:set nu ai ap aw smd showmatch tabstop=4 shiftwidth=4 expandtab:
+
+echo "global-ping" | su - ansible
