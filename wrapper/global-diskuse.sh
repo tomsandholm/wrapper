87f7b945da0ad875e7e78d3715ce162bac47d24e
tom sandholm <tom.sandholm@gmail.com>
2026-01-03 15:20:42 -0500

    update

--- wrapper/global-diskuse.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-#
-echo "global-diskuse" | su - ansible
--- wrapper/global-ping.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-# vi:set nu ai ap aw smd showmatch tabstop=4 shiftwidth=4 expandtab:
-
-echo "global-ping" | su - ansible
